bc2a7cfd21fa114a9ac20105eaa689db40f4b836
Nelson Carvalho <nelsonfilth@hotmail.com>
2024-02-12 15:22:30 -0300

    finalizado to do list

---
+++ src/utils/enums/Tarefas.ts
@@ -0,0 +1,10 @@
+export enum Prioridade {
+  URGENTE = 'urgente',
+  IMPORTANTE = 'importante',
+  NORMAL = 'normal'
+}
+
+export enum Status {
+  PENDENTE = 'pendentes',
+  CONCLUIDA = 'concluída'
+}
